12a56327eebf3f785c5a6d69c94f5f9e4a069200
rick Deakins <rdeakins96@gmail.com>
2022-11-01 19:27:08 -0400

    server created, dependencies updated

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// connect to database
+const db = mysql.createConnection(
+    
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+
+    console.log('Connected to the database!')
+);
+
+
+
+module.exports = db;
